3fa151cdbcb0317ec71aa85ddeaef1eca4134b7e
진석 <ozjinseok@gmail.com>
2025-11-10 14:54:57 +0900

    도전과제3 어드민 페이지 연결하기

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,30 @@ import Users from './pages/admin__users/Users';
 import UserDetail from './pages/admin__users/UserDetail';
 import Settings from './pages/admin__settings/Settings';
 import NotFound from './pages/admin__notfound/NotFound';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-  //1. React Router의 `BrowserRouter`를 설정해야 합니다.
-  //2. 메인 페이지에 Home 컴포넌트를 렌더링 해야 합니다.
-  //3. /admin에서 Dashboard를 렌더링 해야 합니다. 이는 모든 /admin 경로에서 보입니다.
-  //4. /admin/users에서 Users를 렌더링 해야 합니다.
-  //5. /admin/users/:id에서 UsersDetail를 렌더링 해야 합니다.
-  //6. /admin/settings에서 Settings를 렌더링 해야 합니다.
-  //7. /admin/<잘못된 페이지명>에서 NotFound를 렌더링 해야 합니다.
+  return (
+    //1. React Router의 `BrowserRouter`를 설정해야 합니다.
+    //2. 메인 페이지에 Home 컴포넌트를 렌더링 해야 합니다.
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Layout />}>
+          <Route index element={<Dashboard />}></Route>
+          <Route path="/admin/users" element={<Users />}></Route>
+          <Route path="/admin/users/:id" element={<UserDetail />}></Route>
+          <Route path="/admin/settings" element={<Settings />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Route>
+      </Routes>
+    </>
+    //3. /admin에서 Dashboard를 렌더링 해야 합니다. 이는 모든 /admin 경로에서 보입니다.
+    //4. /admin/users에서 Users를 렌더링 해야 합니다.
+    //5. /admin/users/:id에서 UsersDetail를 렌더링 해야 합니다.
+    //6. /admin/settings에서 Settings를 렌더링 해야 합니다.
+    //7. /admin/<잘못된 페이지명>에서 NotFound를 렌더링 해야 합니다.
+  );
 
   return;
 }
